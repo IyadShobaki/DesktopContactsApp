17551435ac8ee511a31ae1b461884a98ff230e1f
IyadShobaki <iyadshobaki82@gmail.com>
2020-08-06 21:21:20 -0400

    Start Creating Contatcts App
    Create WPF UI.
    Adding another window (NewContactWindow) to add contact information to Contact Table.
    Add (sqlite-net-pcl) package.
    Create Contacts database and Contact table using Sqlite statements ( using Contact class).

---
+++ README.md
@@ -0,0 +1,2 @@
+# DesktopContactsApp
+ WPF UI (.NET Framework)- WPF Masterclass. Using SQLite database
---
+++ WpfUI/NewContactWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+using WpfUI.Classes;
+
+namespace WpfUI
+{
+    /// <summary>
+    /// Interaction logic for NewContactWindow.xaml
+    /// </summary>
+    public partial class NewContactWindow : Window
+    {
+        public NewContactWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Contact contact = new Contact()
+            {
+                Name = nameTextBox.Text,
+                Email = emailTextBox.Text,
+                Phone = phoneNumberTextBox.Text
+
+            };
+           
+
+            //SQLiteConnection connection = new SQLiteConnection(databasePath);
+            //connection.CreateTable<Contact>(); // If the table is already exists, it will not created again
+            //connection.Insert(contact);
+            ////connection.Close(); // is not the best way to close connection
+
+            // Better way to close connection is using "using" statement
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<Contact>();
+                connection.Insert(contact);
+          
+            } // will dispose the connection object  and close the connection
+
+            // close NewContactWindow
+            this.Close();
+
+        }
+    }
+}
